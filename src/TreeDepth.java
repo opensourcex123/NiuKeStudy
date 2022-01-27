4955486a2c54f2fbaf5ba1b5feceb2ac5146d4ad
nfx.git <13561592791@163.com>
2022-01-27 19:10:01 +0800

    JZ55 二叉树的深度

---
+++ src/TreeDepth.java
@@ -0,0 +1,28 @@
+// JZ55 二叉树的深度
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class TreeDepth {
+    public int TreeDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
